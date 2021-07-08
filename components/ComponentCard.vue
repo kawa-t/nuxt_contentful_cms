56d12dcc570a2162fc4d08431d0fd86f12f53b1a
河村 泰佑 <kata.kawa813@gmail.com>
2021-07-08 23:11:17 +0900

    見出し

---
+++ components/ComponentCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <article>
+      <div>記事の窓</div>
+    </article>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      reqire: true,
+      default: () => {
+        return {
+          fields: {
+            title: "sample",
+            publishhehAt: new Data(),
+            headerImage: null
+          }
+        };
+      }
+    }
+  },
+  methods: {
+    getFormattedDate(date) {
+      const originDate = new Date(date);
+      const year = originDate.getFullYear();
+      const month = originDate.getMonth() + 1;
+      const day = originDate.getDate();
+      return `${year}年${month}月${day}日`;
+    }
+  }
+};
+</script>
